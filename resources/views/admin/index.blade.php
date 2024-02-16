5d2ac6ea9dd645a60da18f21f70309a239bcd1be
daudtsaqiif <daudtsaqiif49@gmail.com>
2024-02-16 14:48:46 +0700

    1

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -4,4 +4,26 @@
 <div class="container d-flex justify-content-end">
     <a href="{{ route('admin.create') }}" class="btn btn-primary">Create Admin</a>
 </div>
+
+{{-- TABEL USERs --}}
+<div class="container text-center">
+    <h5 class="fw-bold">USERS</h5>
+    <table class="tabel">
+        <tr>
+            <th>No</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Gender</th>
+            <th>Action</th>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    </table>
+</div>
+
 @endsection
